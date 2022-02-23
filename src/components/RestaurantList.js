70f99c25a5cbb351715d0601e7b3325ceb21b151
Aurelie Gedeon <agedeon540@gmail.com>
2022-02-23 10:05:12 -0500

    fetched api

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
 
 export default function RestaurantList() {
+  const [restaurants, setRestaurants] = useState();
+  useEffect(() => {
+    fetch("https://bocacode-intranet-api.web.app/restaurants")
+      .then((res) => res.json())
+      .then((data) => setRestaurants(data))
+      .catch(alert);
+  }, []);
   return <Text>Restuarants</Text>;
 }
